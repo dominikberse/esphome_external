e6e5f9a0177c8f10fa023ac4849e274e07c55ee7
Dominik Berse <dominik.berse@tapdo.io>
2021-12-09 14:23:28 +0100

    fix: single pin and threshold option

--- components/ac_binary_sensor/binary_sensor.py
+++ components/ac_binary_sensor/binary_sensor.py
@@ -2,7 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import pins
 from esphome.components import binary_sensor
-from esphome.const import CONF_ID, CONF_PIN_A, CONF_PIN_B, CONF_FREQUENCY
+from esphome.const import CONF_ID, CONF_PIN, CONF_FREQUENCY,  CONF_THRESHOLD
 
 ACBinarySensor = cg.global_ns.class_(
     'ACBinarySensor', binary_sensor.BinarySensor, cg.Component)
@@ -22,13 +22,13 @@ async def to_code(config):
     await cg.register_component(var, config)
     await binary_sensor.register_binary_sensor(var, config)
 
-    pin_a = await cg.gpio_pin_expression(config[CONF_PIN_A])
-    cg.add(var.add_pin(pin_a))
-
-    pin_b = await cg.gpio_pin_expression(config[CONF_PIN_B])
-    cg.add(var.add_pin(pin_b))
+    pin_a = await cg.gpio_pin_expression(config[CONF_PIN])
+    cg.add(var.set_pin(pin_a))
 
     if CONF_FREQUENCY in config:
         freq = await cg.get_variable(config[CONF_FREQUENCY])
         cg.add(var.set_freq(freq))
 
+    if CONF_THRESHOLD in config:
+        freq = await cg.get_variable(config[CONF_THRESHOLD])
+        cg.add(var.set_threshold(freq))
